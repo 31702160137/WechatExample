e2bf865b07a43af4b989bd4cf0742f55e11aec9f
feng <846783735@qq.com>
2019-01-02 16:45:23 +0800

    first commit

---
+++ app/src/main/java/com/example/a104168/wechatexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a104168.wechatexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
