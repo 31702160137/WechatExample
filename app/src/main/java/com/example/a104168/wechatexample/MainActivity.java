043054c6ac3d7b8baa0b0d140aad77293dad6598
31702160137 <1041680485@qq.com>
2019-01-03 01:29:47 +0800

    add fragments

--- app/src/main/java/com/example/a104168/wechatexample/MainActivity.java
+++ app/src/main/java/com/example/a104168/wechatexample/MainActivity.java
@@ -2,12 +2,81 @@ package com.example.a104168.wechatexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a104168.wechatexample.fragmentlayout.*;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tv_menu_users,tv_menu_chat,tv_menu_my;        //底部导航监听控件
+
+    protected fragment_users fragmentUsers = new fragment_users(); //联系人fragment实例
+    protected fragment_chat fragmentChat   = new fragment_chat();  //聊天室fragment实例
+    protected fragment_my fragmentMy       = new fragment_my();    //个人页fragment实例
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+        initView();
+        initFragmentView();
+    }
+    //初始化fragment容器
+    private void initFragmentView(){
+        this.getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.rl_main,fragmentUsers)
+                .add(R.id.rl_main,fragmentChat)
+                .hide(fragmentChat)
+                .hide(fragmentMy)
+                .show(fragmentUsers)
+                .commit();
+        tv_menu_users.setSelected(true);
+    }
+    //初始化页面
+    private void initView(){
+        tv_menu_chat = findViewById(R.id.tv_nav_chat);
+        tv_menu_my = findViewById(R.id.tv_nav_my);
+        tv_menu_users = findViewById(R.id.tv_nav_users);
+        tv_menu_chat.setOnClickListener(this);
+        tv_menu_my.setOnClickListener(this);
+        tv_menu_users.setOnClickListener(this);
+    }
+    private void selected(){
+        tv_menu_users.setSelected(false);
+        tv_menu_chat.setSelected(false);
+        tv_menu_my.setSelected(false);
+    }
+    @Override
+    public void onClick(View v) {
+        selected();
+        switch (v.getId()){
+            case R.id.tv_nav_chat:
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        .hide(fragmentUsers)
+                        .hide(fragmentMy)
+                        .show(fragmentChat)
+                        .commit();
+                tv_menu_chat.setSelected(true);
+                break;
+            case R.id.tv_nav_users:
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        .hide(fragmentChat)
+                        .hide(fragmentMy)
+                        .show(fragmentUsers)
+                        .commit();
+                tv_menu_users.setSelected(true);
+                break;
+            case R.id.tv_nav_my:
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        .hide(fragmentChat)
+                        .hide(fragmentUsers)
+                        .show(fragmentMy)
+                        .commit();
+                tv_menu_my.setSelected(true);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/a104168/wechatexample/fragmentlayout/fragment_users.java
+++ app/src/main/java/com/example/a104168/wechatexample/fragmentlayout/fragment_users.java
@@ -1,7 +1,9 @@
 package com.example.a104168.wechatexample.fragmentlayout;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +11,11 @@ import android.view.ViewGroup;
 
 import com.example.a104168.wechatexample.R;
 
-public class fragment_chat extends Fragment {
-
+public class fragment_users extends Fragment{
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_chat,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_users,container,false);
         return view;
     }
 }
