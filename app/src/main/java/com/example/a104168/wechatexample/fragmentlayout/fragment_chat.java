46c0dc2100b155b167bcbe265a1538e03937b6a9
31702160137 <1041680485@qq.com>
2019-01-02 20:59:50 +0800

    1.2

---
+++ app/src/main/java/com/example/a104168/wechatexample/fragmentlayout/fragment_chat.java
@@ -0,0 +1,15 @@
+package com.example.a104168.wechatexample.fragmentlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a104168.wechatexample.R;
+
+public class fragment_chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_fragment_chat);
+    }
+}
