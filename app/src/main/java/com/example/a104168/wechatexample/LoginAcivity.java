180a0bfedbda469b5345989baf8630188bb44219
feng <846783735@qq.com>
2019-01-04 10:35:38 +0800

    pagelogin and pageregister

---
+++ app/src/main/java/com/example/a104168/wechatexample/LoginAcivity.java
@@ -0,0 +1,13 @@
+package com.example.a104168.wechatexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_acivity);
+    }
+}
---
+++ app/src/main/java/com/example/a104168/wechatexample/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.a104168.wechatexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
